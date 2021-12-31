0eb0b012d6b0e8566311511289a8de310210d7fc
kgentel <katie.m.gentel@gmail.com>
2021-12-30 20:41:00 -0600

    added projects component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import Nav from './components/navbar/navbar';
 import About from './components/about_me/about_me'
+import Projects from './components/projects/projects'
 
 function App() {
   return (
     <div>
    <Nav />
    <About />
+   <Projects />
    </div>
   );
 }
---
+++ portfolio/src/components/projects/projects.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Card, Col } from "react-bootstrap";
+import Marvel from "../../assets/project-1.jpeg";
+import Tracker from "../../assets/employee-tracker.jpg";
+import SkinDeep from "../../assets/skin-deep.jpeg";
+import "./projects.css";
+
+function projects() {
+  return (
+    <div className="projects-section">
+      <div class="section-title">
+        <h2>Projects</h2>
+      </div>
+      <Row>
+        <Col>
+          <a href="https://ska246801.github.io/Marvel-Quiz-Project-1/" target="_blank" className="link">
+            <Card.Img variant="top" src={Marvel} />
+          </a>
+          <Card.Body>
+            <Card.Title className="project-title">Marvel Character Quiz</Card.Title>
+            <Card.Text>
+              An interactive personality quiz that matches the user to a Marvel
+              superhero most like themselves.
+            </Card.Text>
+          </Card.Body>
+        </Col>
+        <Col>
+          <a href="https://www.youtube.com/watch?v=MdGaauJyMSc" target="_blank" className="link">
+            <Card.Img variant="top" src={Tracker} />
+          </a>
+          <Card.Body>
+            <Card.Title className="project-title">Employee Tracker</Card.Title>
+            <Card.Text>
+              A tracker of tables with department, role and employee data with
+              the ability to add or edit information.
+            </Card.Text>
+          </Card.Body>
+        </Col>
+        <Col>
+          <a href="https://mswil-skindeep.herokuapp.com/" target="_blank" className="link">
+            <Card.Img variant="top" src={SkinDeep} />
+          </a>
+          <Card.Body>
+            <Card.Title className="project-title">Skin Deep</Card.Title>
+            <Card.Text>
+              A social media website that connects tattoo artists with
+              enthusiasts.
+            </Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default projects;
